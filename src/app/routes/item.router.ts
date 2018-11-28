3f8d49f4f485230b248a155cc3d4fa917ebaca63
Freek Mencke <info@toxsickproductions.com>
2018-11-28 19:34:32 +0100

    added editor config

--- src/app/routes/item.router.ts
+++ src/app/routes/item.router.ts
@@ -4,36 +4,36 @@ import { ItemRepository } from '../repositories/item.repository';
 
 export class ItemRouter {
 
-    static create(app: express.Application): void {
-        const router = express.Router();
+  static create(app: express.Application): void {
+    const router = express.Router();
 
-        this.getItem(router);
-        this.getItems(router);
+    this.getItem(router);
+    this.getItems(router);
 
-        app.use('/item', router);
-    }
+    app.use('/item', router);
+  }
 
-    private static getItem(router: express.Router): void {
-        router.get('/:id', (req, res) => {
-            API.getDbConnection(connection =>
-                ItemRepository.getItem(req.params.id, connection).then(({ statusCode, items }) => {
-                    res.status(statusCode);
-                    res.send(items);
-                })
-            );
-        });
+  private static getItem(router: express.Router): void {
+    router.get('/:id', (req, res) => {
+      API.getDbConnection(connection =>
+        ItemRepository.getItem(req.params.id, connection).then(({ statusCode, items }) => {
+          res.status(statusCode);
+          res.send(items);
+        })
+      );
+    });
 
-    }
+  }
 
-    private static getItems(router: express.Router): void {
-        router.get('/', (req, res) => {
-            API.getDbConnection(connection =>
-                ItemRepository.getItems(req.query.query, connection).then(({ statusCode, items }) => {
-                    res.status(statusCode);
-                    res.send(items);
-                })
-            );
-        });
-    }
+  private static getItems(router: express.Router): void {
+    router.get('/', (req, res) => {
+      API.getDbConnection(connection =>
+        ItemRepository.getItems(req.query.query, connection).then(({ statusCode, items }) => {
+          res.status(statusCode);
+          res.send(items);
+        })
+      );
+    });
+  }
 
 }
